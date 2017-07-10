3afa2e5fad10ac1f7e8e3a11f7598bf0461f09b8
Larry Davis <lawdavis@adobe.com>
2017-07-10 16:04:08 -0700

    @releng - Publish spectrum-css#2.0.0-beta.17 docs

---
+++ docs/js/vendor/typekit.js
@@ -0,0 +1,41 @@
+/* global Typekit, document */
+/* jshint -W033,-W116 */
+(function(window, undefined) {
+  "use strict"
+
+  // note: 'ruf7eed' is CoralUI's default kit
+  var config = {
+    kitId: 'ruf7eed',
+    scriptTimeout: 3000
+  };
+
+  if (!window.Typekit) { // we load the typescript only once
+    var h = document.getElementsByTagName("html")[0];
+    h.className += " wf-loading";
+    var t = setTimeout(function() {
+      h.className = h.className.replace(/(\s|^)wf-loading(\s|$)/g, " ");
+      h.className += " wf-inactive";
+    }, config.scriptTimeout);
+    var tk = document.createElement("script"),
+      d = false;
+
+    // Always load over https
+    tk.src = 'https://use.typekit.net/' + config.kitId + '.js'
+    tk.type = "text/javascript";
+    tk.async = "true";
+    tk.onload = tk.onreadystatechange = function() {
+      var a = this.readyState;
+      if (d || a && a !== "complete" && a !== "loaded") {
+        return;
+      }
+      d = true;
+      clearTimeout(t);
+      try {
+        Typekit.load(config);
+      } catch (b) {}
+    };
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(tk, s);
+  }
+
+}(this));
