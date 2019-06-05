c0c93c11ba6a6aeb946c4c76781a8153793ece0a
Larry Davis <lawdavis@adobe.com>
2019-06-05 09:16:53 -0700

    Make everything all draggable and stuff

---
+++ topdoc/resources/js/draggable.js
@@ -0,0 +1,46 @@
+(function() {
+  let startX = 0, startY = 0;
+  let offsetX = 0, offsetY = 0;
+  let currentElement = null;
+  let currentZIndex = 10;
+
+  document.addEventListener('mousedown', startDragging);
+
+  function finishDragging(e) {
+    document.removeEventListener('mouseup', finishDragging);
+    document.removeEventListener('mousemove', handleDrag);
+    currentElement = null;
+  }
+
+  function handleDrag(e) {
+    e.preventDefault();
+    currentElement.style.top = (e.clientY - offsetY) + "px";
+    currentElement.style.left = (e.clientX - offsetX) + "px";
+  }
+
+  function startDragging(e) {
+    if (e.target.hasAttribute('dragHandle')) {
+      // Find if the element or anyone up the tree is draggable
+      let element = e.target.closest('[draggablePanel]');
+
+      if (element) {
+        e.preventDefault();
+        currentElement = element;
+
+        offsetY = e.clientY - currentElement.offsetTop;
+        offsetX = e.clientX - currentElement.offsetLeft;
+
+        currentElement.style.position = 'fixed';
+        currentElement.style.zIndex = ++currentZIndex;
+        document.body.appendChild(currentElement);
+
+        if (currentElement.classList.contains('spectrum-Panel')) {
+          currentElement.classList.add('spectrum-Panel--floating');
+        }
+
+        document.addEventListener('mouseup', finishDragging);
+        document.addEventListener('mousemove', handleDrag);
+      }
+    }
+  }
+}());
